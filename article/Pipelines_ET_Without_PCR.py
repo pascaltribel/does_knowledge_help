219fcaf14fbfad1ac3f0686883c65d4d1d91141b
pascaltribel <pascal.tribel@ulb.be>
2026-03-11 13:27:05 +0100

    update

---
+++ article/Pipelines_ET_Without_PCR.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm.auto import tqdm
+from utils import rnmse
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LinearRegression
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
+from sklearn.compose import TransformedTargetRegressor
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import Subset
+from neuralop import FNO
+import pandas as pd
+import seaborn as sns
+import matplotlib
+from time import time
+sns.set_style('whitegrid')
+sns.set_context('notebook')
+sns.set_palette('hot', n_colors=7)
+plt.rc('text', usetex=True)      
+
+font = {'family' : 'serif',
+        'weight' : 'bold',
+        'size'   : 22}
+
+matplotlib.rc('font', **font)
+
+plt.rc('xtick',labelsize=12)
+plt.rc('ytick',labelsize=12)
+
+
+# In[2]:
+
+print("Reading files...")
+x = torch.cat([torch.load("../dataset/x_train.pt"), torch.load("../dataset/x_train_2.pt")])
+y = torch.cat([torch.load("../dataset/y_train.pt"), torch.load("../dataset/y_train_2.pt")])
+#c = torch.cat([torch.load("../dataset/c_train.pt"), torch.load("../dataset/c_train_2.pt")])
+
+
+# In[3]:
+
+n_points = x.shape[0]
+n_cv = 5
+n_epochs = 50
+n_components_pca = 512
+scores = {}
+
+# In[4]:
+
+
+x_np, y_np = x.numpy(), y.numpy()
+x_np_reshaped, y_np_reshaped = x_np.reshape((x_np.shape[0], -1)), y_np.reshape((y_np.shape[0], -1))
+#x_test_np, y_test_np, c_test_np = x_test.numpy(), y_test.numpy(), c_test.numpy()
+#x_test_np_reshaped, y_test_np_reshaped, c_test_np_reshaped = x_test_np.reshape((x_test_np.shape[0], -1)), y_test_np.reshape((y_test_np.shape[0], -1)), c_test_np.reshape((c_test_np.shape[0], -1))
+
+
+class VanillaRegressor(BaseEstimator):
+    def __init__(self, regressor, n_components=64):
+        self.regressor = regressor
+        self.n_components = n_components
+        self.pipe = TransformedTargetRegressor(
+            regressor=Pipeline([
+                ("pca", PCA(n_components=self.n_components)),
+                ("reg", self.regressor)
+            ]),
+            transformer=PCA(n_components=self.n_components),
+            check_inverse=False
+        )
+
+    def fit(self, X, y):
+        self.pipe.fit(X, y)
+
+    def predict(self, X):
+        return self.pipe.predict(X)
+
+def scorer_rnmse(estimator, x, y):
+    return rnmse(estimator.predict(x), y)
+
+
+
+# In[17]:
+
+t = time()
+print("Vanilla ET...")
+from sklearn.ensemble import ExtraTreesRegressor
+scores["ET Without PCR"] = cross_val_score(
+    VanillaRegressor(TransformedTargetRegressor(
+            regressor=Pipeline([
+                ("pca", PCA(n_components=n_components_pca)),
+                ("et", ExtraTreesRegressor(n_jobs=-1, verbose=1))
+            ]),
+            transformer=PCA(n_components=n_components_pca),
+            check_inverse=False
+        ), n_components_pca),
+    x_np_reshaped[:n_points], y_np_reshaped[:n_points],
+    cv=n_cv,
+    scoring=scorer_rnmse,
+    verbose=1
+)
+
+print(f"Done in {time()-t}s")
+
+print("Saving data...")
+
+data = pd.DataFrame(scores)[sorted(scores, key=lambda x: np.mean(scores[x]), reverse=True)]
+data.to_csv('results_et_without_pcr.csv')
+
+print("Done.")
